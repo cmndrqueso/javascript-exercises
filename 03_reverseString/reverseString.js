3daccf2291eb5cd5c689b1e4eaafdcb620d690aa
Andres Calzadias <acalzadias467@icloud.com>
2022-03-26 09:42:27 -0700

    Reverse String done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+// We have to reverse a sting of the
+// convert a string into an array (split() method)
+//get array length
+//starts a lopop from the last array index num (Array.lenght -1)
+// Pops the last array element 
+//Creates new array and push() the popped item
+
+
+const reverseString = function(string) {
+    //const stringArray = string.split('');
+    const reverseStringArray = [];
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseStringArray.push(string[i]);
+    }
+    return reverseStringArray.join('');
 
 };
+// 10 
+//
+
 
 // Do not edit below this line
 module.exports = reverseString;
