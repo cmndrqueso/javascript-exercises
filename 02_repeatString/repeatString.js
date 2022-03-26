1b841371a1a5785d91a72400b2f299ca0f429110
Andres Calzadias <acalzadias467@icloud.com>
2022-03-25 18:42:56 -0700

    completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const text = [];
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        text.push(string);
+    }
+    return text.join('');
 
 };
 
