7dd6c5ce4f0b74bac1478cb2a52ce4d3de2f9bed
Andres Calzadias <acalzadias467@icloud.com>
2022-03-26 18:43:37 -0700

    Temp conversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  const formula = ((fTemp-32)*(5/9));
+  const roundedConversion = Math.round(formula * 10) / 10;
+  return roundedConversion;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  const formula = (((9/5)*cTemp)+32);
+  const roundedConversion = Math.round(formula * 10) / 10;
+  return roundedConversion;
 };
 
 // Do not edit below this line
