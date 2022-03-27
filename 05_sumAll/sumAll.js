e307271892a3f46191607cc504b8bccacd4a7fe4
Andres Calzadias <acalzadias467@icloud.com>
2022-03-26 17:43:18 -0700

    sum all complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+    if(x < 0 || y < 0) {
+        return 'ERROR';
+    }
+    else if (typeof x !== 'number' || typeof y !== 'number') {
+        return 'ERROR';
+    }
+    else if (x < y) {
+        start = x;
+        end = y;
+    }
+    else if (x > y){
+        start = y;
+        end = x;
+    }
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
