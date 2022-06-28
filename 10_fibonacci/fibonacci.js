320efd75e442802ce99500632650ce809519d899
Andres Calzadias <acalzadias467@icloud.com>
2022-06-28 00:13:32 -0700

    Completed several excercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let f = 1;
+    for( let i = 0; i < a; i++) {
+        f += i;
+    }
+    return f
 };
 
 // Do not edit below this line
