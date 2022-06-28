95a7643d3c62f2b3e722baabb24296f8623a4758
Andres Calzadias <acalzadias467@icloud.com>
2022-06-27 23:45:55 -0700

    Completed calculator excercises, all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
 const add = function() {
-	
+  let x = arguments[0];
+  let y = arguments[1];
+  return x + y;
 };
 
 const subtract = function() {
-	
+	let x = arguments[0];
+  let y = arguments[1];
+  return x -y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((x, y) => x + y, 0);
+}
+
+const multiply = function(array) {
+  return array.length
+  ? array.reduce((x, y) => x * y)
+  : 0;
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	return a**b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for (let i = a; i > 0; i--){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
